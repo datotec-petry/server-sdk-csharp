753fa00ee4f96db8414d64302813af06fb0c2f47
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-22 15:14:33 +0000

    Release 0.4.1

--- src/Vapi.Net/VapiClient.cs
+++ src/Vapi.Net/VapiClient.cs
@@ -16,7 +16,7 @@ public partial class VapiClient
                 { "X-Fern-Language", "C#" },
                 { "X-Fern-SDK-Name", "Vapi.Net" },
                 { "X-Fern-SDK-Version", Version.Current },
-                { "User-Agent", "Vapi.Net/0.4.0" },
+                { "User-Agent", "Vapi.Net/0.4.1" },
             }
         );
         clientOptions ??= new ClientOptions();
